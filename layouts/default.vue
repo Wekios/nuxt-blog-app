14c80ea089c549a3cc77d4e85e13046e8f8b2468
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-07-26 16:52:58 +0200

    blog app so far

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,15 +6,18 @@
 </template>
 
 <script>
-import Header from "~/components/Header.vue";
 
 export default {
-  components: {
-    Header
-  }
 };
 </script>
 
 
 <style>
+html {
+  font-family: 'Open Sans', sans-serif;
+}
+
+body {
+  margin: 0;
+}
 </style>
